b4590fcf1f1357585efbbb8146b7f89815bc6dd1
dkwilson <general@ytechnology.net>
2019-05-01 01:42:02 -0400

    front-end

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+import AddResource from "./components/addResource.component";
+import EditResource from "./components/edit-resource.component";
+import ResourceList from "./components/resourceList.component";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import logo from "./logo.png";
+
+class App extends Component {
+  render(){
+    return (
+      <Router>
+          <div className="container">
+            <nav className="navbar navbar-expand-lg navbar-light">
+              <a class="navbar-brand" href="/" target="_blank">
+                <img src={logo} width="100" height="100" alt="" />
+              </a>
+              <Link to="/" className="navbar-brand">Chunky- Learning Resource Tracker</Link>
+              <div className="collpase navbar-collapse">
+                <ul className="navbar-nav mr-auto">
+                  <li className="navbar-item">
+                    <Link to="/" className="nav-link">Resources</Link>
+                  </li>
+                  <li className="navbar-item">
+                    <Link to="/create" className="nav-link">Add Resource</Link>
+                  </li>
+                </ul>
+              </div>
+            </nav> 
+
+          <Route path="/" exact component={ResourceList} />
+          <Route path="/edit/:id" component={EditResource} />
+          <Route path="/create" component={AddResource} />
+          </div>
+      </Router>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/components/resourceList.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ResourceList extends Component {
+    render() {
+        return (
+            <div>
+                <p>Welcome to Resource List Component!!</p>
+            </div>
+        )
+    }
+}
